a28817b4852d7a61323676739e7cd9e5e12faeb8
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-03-13 19:26:49 -0400

    Code 03-13-24 7:26 PM

--- smallbot.cpp
+++ smallbot.cpp
@@ -3,25 +3,50 @@
 int main()
 {
     printf("Hello World\n");
+  // Lift and Open Claw
     set_servo_position(1,250);
     set_servo_position(2,170);
+  // Drive Forward 
     motor(1, -60);
     motor(2, -70);
     msleep(2750);
-    set_servo_position(1,1500);
+  // Get wooden person
+    set_servo_position(1,1450);   
     motor(1,0);
     motor(2,0);
     msleep(500);
-    set_servo_position(2,1780);
+  //Grab Person
+    set_servo_position(2, 1830);
     msleep(1000);
-    set_servo_position(1,250);
+  //Lift Person
+    set_servo_position(1, 350);
     msleep(750);
-    motor(1,-1000);
+  //Drive curved
+    motor(1,-500);
     motor(2,-100);
-    msleep(3000);
-    motor(1,-50);
-    motor(2,1);
-    msleep(500);
+    msleep(750);
+  //Turn Left
+    motor(1, -1000);
+    motor (2, -10);
+    msleep(500); 
+   //Move forward
+    motor(1,-550);
+    motor(2,-480);
+    msleep(5350);
+    //Lower Claw
+    motor(1,0);
+    motor(2,0);
+    msleep(100);
+    set_servo_position(1,1175);
+    msleep(50);
+    set_servo_position(1,1290);  
+    msleep(1000);
+    set_servo_position(2,250);
+    msleep(50);
+    
+    //End of "Raising Flag" Mission
+    
+    //Flip the Switch Mission
    return 0;
 
     
