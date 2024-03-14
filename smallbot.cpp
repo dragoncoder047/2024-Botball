e8bf4b352881c20965fad1135b35a55e8fdb6270
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-03-13 21:13:33 -0400

    Smallbot update 9:13 PM

--- smallbot.cpp
+++ smallbot.cpp
@@ -7,32 +7,32 @@ int main()
     set_servo_position(1,250);
     set_servo_position(2,170);
   // Drive Forward 
-    motor(1, -60);
-    motor(2, -70);
-    msleep(2750);
+    motor(1, -80);
+    motor(2, -90);
+    msleep(2895);
   // Get wooden person
-    set_servo_position(1,1450);   
+    set_servo_position(1,1720);   
     motor(1,0);
     motor(2,0);
-    msleep(500);
+    msleep(400);
   //Grab Person
-    set_servo_position(2, 1830);
-    msleep(1000);
+    set_servo_position(2, 2830);
+    msleep(1100);
   //Lift Person
     set_servo_position(1, 350);
     msleep(750);
   //Drive curved
-    motor(1,-500);
+    motor(1,-100);
     motor(2,-100);
-    msleep(750);
+    msleep(800);
   //Turn Left
     motor(1, -1000);
     motor (2, -10);
     msleep(500); 
    //Move forward
-    motor(1,-550);
-    motor(2,-480);
-    msleep(5350);
+    motor(1,-1000);
+    motor(2,-580);
+    msleep(5400);
     //Lower Claw
     motor(1,0);
     motor(2,0);
